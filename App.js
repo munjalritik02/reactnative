547804e89fd4c3737d6d8ab49d33b8ec7d76d105
munjalritik02 <munjal.ritik02@gmail.com>
2022-03-03 23:35:14 +0530

    commit

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text,Button, TouchableWithoutFeedback, Image, View, SafeAreaView, Platform, Alert } from 'react-native';
+import { TouchableNativeFeedback } from 'react-native-web';
+import {useDimensions} from '@react-native-community/hooks';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  console.log(useDimensions)
+   return (
+  
+
+    <SafeAreaView style={styles.container}>
+         <view style={{
+          backgroundcolor: "dodgerblue",
+          flex:1,
+          flexDirection: "row",
+        }} ></view>
+      <Text > Hello React native!</Text>
+      <TouchableWithoutFeedback onPress={() => console.log("Image tapped")}>
+      <Image 
+            source={{
+            width: 200,
+            height: 300,
+            uri: "https://image.shutterstock.com/image-photo/weather-forecast-concept-climate-change-260nw-1390294940.jpg",
+          }}
+          />
+      </TouchableWithoutFeedback>
+       
+       
+      <StatusBar style="auto" /> 
+      <Button 
+      color="orange"
+      title="click me"
+       onPress={() => Alert.alert("My title", "My message", [
+  
+         {text: "yes", onPress: () => console.log("Yes")},
+         {text: "No",  onPress: () => console.log("No")},
+
+       ])} />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingTop: Platform.os === "android"  ? 20: 0,
+    
   },
 });
